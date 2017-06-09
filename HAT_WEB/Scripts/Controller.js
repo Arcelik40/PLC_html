c705700dca4bbd29e7053a7977ed1311d55a9248
ARcelık <cihan.sebzeci@tufts.edu>
2017-06-09 11:37:24 +0400

    first commit

---
+++ HAT_WEB/Scripts/Controller.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $(".myLightButton").removeClass("btn-danger").addClass("btn-default");
+    $(".myLightButton").removeClass("btn-success").addClass("btn-default");
+    $(".myLightButton").removeClass("btn-warning").addClass("btn-default");
+
+    $('#chkIleri').change(function () {
+        alert($(this).val());
+    });
+
+    $('#tampon').removeClass("btn-success").addClass("btn-danger");
+});
+function onClickDanger()
+{
+    $('#StatusDanger').addClass("btn-danger");
+}
+function onClickWarning() {
+    $('#StatusWarning').addClass("btn-warning");
+}
+function onClickSuccess() {
+    $('#StatusSuccess').addClass("btn-success");
+}
---
+++ HAT_WEB/Scripts/plc.js
@@ -0,0 +1,45 @@
+﻿ $(document).ready(function(){
+        //query the lamba variable every second
+        $.ajaxSetup({ cache: false });
+	
+        init(); 
+    });
+		
+	function init(){
+		  	
+		    $("#btnDur").click(function(){    
+                    gonder(true,'"WEB_Server".Sistem_Stop"');
+					gonder(false,'"WEB_Server".Sistem_Start"');	
+					gonder(false,'"WEB_Server".Genel_Reset"');					
+           });
+		   $("#btnGit").click(function(){    
+                    gonder(true,'"WEB_Server".Sistem_Start"');	
+					gonder(false,'"WEB_Server".Sistem_Stop"');	
+					gonder(false,'"WEB_Server".Genel_Reset"');					
+           });
+		   $("#btnRst").mousedown(function(){    
+                    gonder(true,'"WEB_Server".Genel_Reset"');	
+							
+           });
+		   		   $("#btnRst").mouseup(function(){    
+                    gonder(false,'"WEB_Server".Genel_Reset"');	
+							
+           });
+		   
+	}
+	function gonder(val,name) {
+					url="yaz.htm";
+					$('#tempData').val("0");
+					console.log(val);
+					if(val==true)
+						$('#tempData').val("1");
+						sdata=escape(name)+'='+$('#tempData').val();;
+						console.log(sdata);
+					if(val==false)
+						$('#tempData').val("0");
+                    sdata=escape(name)+'='+$('#tempData').val();;
+                    $.post(url,sdata,function(result){});
+
+					
+	}
+ // plc yazma bitişi
